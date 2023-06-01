049e16eab98b0bf67e4a176008e82fc37fe9ba50
Valerian Coelho <valerian27012004@gmail.com>
2023-06-01 17:52:30 +0530

    Report Function Added

--- App.py
+++ App.py
@@ -1,7 +1,40 @@
 from customtkinter import *
 
+def optionmenu_callback(choice):
+    print("optionmenu dropdown clicked:", choice)
+
 def report():
-    print('Report')
+    def submitReport():
+        pass
+
+    homeFrame.pack_forget()
+
+    reportFrame = CTkFrame(window)
+    reportFrame.pack(padx = 20, pady = 20)
+
+    reportField = CTkLabel(reportFrame, text = "Report Natural Disaster", font=CTkFont(size=15, weight="bold"))
+    reportField.grid(padx = 10, pady = 10, row = 0, column = 0, columnspan=2)
+
+    disasterTypeField = CTkLabel(reportFrame, text = "Natural Disaster Type: ")
+    disasterTypeField.grid(padx = 10, pady = 10, row = 1, column = 0)
+
+    disasterTypeInput = CTkEntry(reportFrame, placeholder_text = "Earthquake", width=200)
+    disasterTypeInput.grid(padx = 10, pady = 10, row = 1, column = 1)
+
+    disasterLocationField = CTkLabel(reportFrame, text = "Natural Disaster Location: ")
+    disasterLocationField.grid(padx = 10, pady = 10, row = 2, column = 0)
+
+    disasterLocationInput = CTkEntry(reportFrame, placeholder_text = "Japan", width=200)
+    disasterLocationInput.grid(padx = 10, pady = 10, row = 2, column = 1)
+
+    disasterMagnitudeField = CTkLabel(reportFrame, text = "Natural Disaster Severity: ")
+    disasterMagnitudeField.grid(padx = 10, pady = 10, row = 3, column = 0)
+
+    disasterMagnitudeInput = CTkEntry(reportFrame, placeholder_text = "Low, Moderate, High, Extreme", width=200)
+    disasterMagnitudeInput.grid(padx = 10, pady = 10, row = 3, column = 1)
+
+    submitReportBtn = CTkButton(reportFrame, text = "Submit Report", command=submitReport)
+    submitReportBtn.grid(padx = 10, pady = 10, row = 4, column = 0, columnspan=2)
     
 def incident():
     print('Incident')
@@ -9,28 +42,28 @@ def incident():
 def donate():
     print('Donate')
 
-def resource():
+def donations():
     print('Resources')
 
 
 window = CTk()
 
-mainFrame = CTkFrame(window)
-mainFrame.pack(padx = 20, pady = 20)
+homeFrame = CTkFrame(window)
+homeFrame.pack(padx = 20, pady = 20)
 
-titleField = CTkLabel(mainFrame, text = "Natural Disaster Management", width = 100, font=CTkFont(size=25, weight="bold"))
+titleField = CTkLabel(homeFrame, text = "Natural Disaster Management", font=CTkFont(size=25, weight="bold"))
 titleField.grid(padx = 10, pady = (25, 10), row = 0, column = 0, columnspan=2)
 
-reportBtn = CTkButton(mainFrame, text = "Report Natural Disaster", command=report, width=150, height=50)
+reportBtn = CTkButton(homeFrame, text = "Report Natural Disaster", command=report, width=150, height=50)
 reportBtn.grid(padx = 30, pady = 20, row = 1, column = 0)
 
-reportBtn = CTkButton(mainFrame, text = "View Incident Log", command=incident, width=150, height=50)
-reportBtn.grid(padx = 30, pady = 20, row = 1, column = 1)
+incidentBtn = CTkButton(homeFrame, text = "View Incident Log", command=incident, width=150, height=50)
+incidentBtn.grid(padx = 30, pady = 20, row = 1, column = 1)
 
-reportBtn = CTkButton(mainFrame, text = "Donate Resources", command=donate, width=150, height=50)
-reportBtn.grid(padx = 30, pady = (15, 30), row = 2, column = 0)
+donateBtn = CTkButton(homeFrame, text = "Donate Resources", command=donate, width=150, height=50)
+donateBtn.grid(padx = 30, pady = (15, 30), row = 2, column = 0)
 
-reportBtn = CTkButton(mainFrame, text = "Check Resource Availability", command=resource, width=150, height=50)
-reportBtn.grid(padx = 30, pady = (15, 30), row = 2, column = 1)
+resourceBtn = CTkButton(homeFrame, text = "Check Resource Availability", command=donations, width=150, height=50)
+resourceBtn.grid(padx = 30, pady = (15, 30), row = 2, column = 1)
 
 window.mainloop()
